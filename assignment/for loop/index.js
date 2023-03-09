fda5d9ad002f42e6b33eeea27556de3ff4c8016c
BenjaminAJ <ajanakub30@gmail.com>
2023-03-09 09:14:42 +0100

    modified the generatetimestable function

--- assignment/for loop/index.js
+++ assignment/for loop/index.js
@@ -3,10 +3,16 @@ const lists = document.getElementById('lists');
 function generateTimesTable() {
     let from = document.getElementById('from').value;
     let to = document.getElementById('to').value;
+    if (+from > +to){
+        alert('Insert valid numbers');
+    }
+    else if(lists.innerText != ''){
+        lists.innerHTML= '';
+    }
     for (+from; +from <= +to; +from++) {
         for (let index = 1; index < 13; index++) {
-            lists.innerHTML+= `<p> ${+from} * ${index} = ${+from * index}</p>`
+            lists.innerHTML+= `<p> ${+from} * ${index} = ${+from * index}</p>`;
         }
-        lists.innerHTML+= `<hr><br>`
+        lists.innerHTML+= `<hr><br>`;
     }
 }
