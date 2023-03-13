1bde295226f954d9f8706c81ca6bbdc7410caa71
BenjaminAJ <ajanakub30@gmail.com>
2023-03-13 11:48:06 +0100

    made some modifications to the namelist

--- assignment/namelist/script.js
+++ assignment/namelist/script.js
@@ -20,7 +20,34 @@ function loop() {
     listElement.innerHTML = '';
     for (let i = 0; i < studentList.length; i++) {
         let element = studentList[i];
-        listElement.innerHTML += `<li> ${element} </li>`;
-        console.log('hey');
+        listElement.innerHTML += `<div> <p> ${i +1}. ${element} </p> <button class='btn btn-primary' onclick='deleteItem(${i})'>Del</button> </div>`;
     }    
 }
+
+function deleteItem(index) {
+    if (index == '0'){
+        studentList.shift();
+        loop();
+
+    }
+    else{
+        studentList.splice((+index), 1);
+        loop();
+    }
+}
+
+let array = [1, 2, 2, 3, 4, 5, 4, 6, 10]
+function truncate(array) {
+    let oldarray = array;
+    let newarray = [];
+    for (let index = 0; index < oldarray.length; index++) {
+        if (!newarray.includes(`${oldarray[index]}`)) {
+            newarray.push(`${oldarray[index]}`);
+        }
+        
+    }
+    console.log(oldarray);
+    console.log(newarray);
+}
+
+truncate(array)
